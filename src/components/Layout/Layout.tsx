88bd9664c57cddaf36b51c6b2ad224d8bc896e6a
robrechtme <robrechtmeersman@hotmail.com>
2020-12-29 20:48:31 +0100

    ✨ Add footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Layout.module.css';
 import { ReactComponent as Back } from '../../assets/back.svg';
+import { ReactComponent as GitHubIcon } from '../../assets/github.svg';
 
 type Props = {
   backButtonURL?: string;
@@ -9,17 +10,20 @@ type Props = {
 };
 
 const Layout: React.FC<Props> = ({ children, backButtonURL, backButtonText = 'Back' }) => (
-  <div className={styles.container}>
-    {backButtonURL && (
-      <Link to={backButtonURL} className={styles.backButton}>
-        <span className={styles.backButtonIcon}>
-          <Back />
-        </span>
-        {backButtonText}
-      </Link>
-    )}
-    {children}
-  </div>
+  <>
+    <div className={styles.container}>
+      {backButtonURL && (
+        <Link to={backButtonURL} className={styles.backButton}>
+          <span className={styles.backButtonIcon}>
+            <Back />
+          </span>
+          {backButtonText}
+        </Link>
+      )}
+      {children}
+    </div>
+    <footer className={styles.footer}>View on <a href="https://github.com/robrechtme/ghent-parkings" target="_blank" rel="noreferrer"><GitHubIcon className={styles.footerIcon} /> GitHub</a></footer>
+  </>
 );
 
 export default Layout;
